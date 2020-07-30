823a81e80a7ff04882898bd4e62e8544e82fd2d5
Dustin Brisebois <dustin.brisebois@lambdasolutions.net>
2020-07-30 10:55:04 -0700

    fix manage.php

--- manage.php
+++ manage.php
@@ -17,164 +17,73 @@
 /**
  * autogroup local plugin
  *
- * A user object relates to a real Moodle user; it acts as a container
- * for multiple courses which in turn contain multiple groups.
- * Initialising a course object will automatically load each autogroup
- * group which could be relevant for a user into memory.
- *
- * A user is also a group member; a membership register is also maintained
- * by this class.
+ * A course object relates to a Moodle course and acts as a container
+ * for multiple groups. Initialising a course object will automatically
+ * load each autogroup group for that course into memory.
  *
  * @package    local
  * @subpackage autogroup
  * @author     Mark Ward (me@moodlemark.com)
- * @date       December 2014
+ * @date       April 2015
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-namespace local_autogroup\domain;
-
-use local_autogroup\domain;
-use local_autogroup\exception;
-
 /**
- * Class user
- *
- * Wraps a standard moodle user with additional helper functions, linking
- * to the users courses and on to their autogroups.
- *
- * TODO: Some of the functionality here belongs in a repository class
- *
- * @package local_autogroup\domain
+ * This file allows users with the correct capability to manage
+ * grouping logic for autogroups within a course.
  */
-class user extends domain
-{
-    /**
-     * @param object|int $user
-     * @param \moodle_database $db
-     * @param int $onlyload a courseid to restrict loading to
-     * @throws exception\invalid_user_argument
-     */
-    public function __construct ($user, \moodle_database $db, $onlyload = 0)
-    {
-        //get the data for this user
-        $this->parse_user_data($user, $db);
 
-        //register which autogroup groups this user is a member of currently
-        $this->get_group_membership($db);
+namespace local_autogroup;
 
-        //if applicable, load courses this user is on and their autogroup groups
-        $this->get_courses($db, $onlyload);
+require_once(dirname(__FILE__) . '/pageinit.php');
 
-        return true;
-    }
+use \local_autogroup\domain;
+use \local_autogroup\form;
+use \local_autogroup\usecase;
+use \local_autogroup_renderer;
+use \moodle_url;
+use \context_course;
+use \stdClass;
 
-    /**
-     * @param \moodle_database $db
-     * @return bool
-     */
-    public function verify_user_group_membership(\moodle_database $db)
-    {
-        $result = true;
-        foreach ($this->courses as $course){
-            $result &= $course->verify_user_group_membership($this->object, $db);
-        }
+$courseid = required_param('courseid', PARAM_INT);
+$context = context_course::instance($courseid);
 
-        $this->get_group_membership($db);
+require_capability('local/autogroup:managecourse', $context);
 
-        return $result;
-    }
+global $PAGE, $DB, $SITE;
 
-    /**
-     * Get courses for this user where an autogroup set has been added
-     *
-     * @param \moodle_database $db
-     */
-    private function get_courses(\moodle_database $db, $onlyload = 0)
-    {
-        if($onlyload < 1) {
-            $sql = "SELECT e.courseid" . PHP_EOL
-                . "FROM {enrol} e" . PHP_EOL
-                . "LEFT JOIN {user_enrolments} ue" . PHP_EOL
-                . "ON ue.enrolid = e.id" . PHP_EOL
-                . "LEFT JOIN {local_autogroup_set} gs" . PHP_EOL
-                . "ON gs.courseid = e.courseid" . PHP_EOL
-                . "WHERE ue.userid = :userid" . PHP_EOL
-                . "AND gs.id IS NOT NULL";
-            $param = array('userid' => $this->id);
-
-            $this->courses = $db->get_fieldset_sql($sql, $param);
-        }
-
-        else {
-            $this->courses[] = $onlyload;
-        }
-
-        foreach($this->courses as $k => $courseid){
-            try {
-                $courseid = (int) $courseid;
-                $this->courses[$k] = new course($courseid, $db);
-            } catch (exception\invalid_course_argument $e){
-                unset($this->courses[$k]);
-            }
-        }
-    }
-
-    /**
-     * @param \moodle_database $db
-     */
-    private function get_group_membership(\moodle_database $db)
-    {
-        $sql = "SELECT g.id, g.courseid".PHP_EOL
-              ."FROM {groups} g".PHP_EOL
-              ."LEFT JOIN {groups_members} gm".PHP_EOL
-              ."ON gm.groupid = g.id".PHP_EOL
-              ."WHERE gm.userid = :userid".PHP_EOL
-              ."AND ".$db->sql_like('g.idnumber', ':autogrouptag');
-        $param = array(
-            'userid' => $this->id,
-            'autogrouptag' => 'autogroup|%'
-        );
-
-        $this->membership = $db->get_records_sql_menu($sql,$param);
-    }
-
-    /**
-     * @param object|int $user
-     * @return bool
-     * @throws exception\invalid_user_argument
-     */
-    private function parse_user_data ($user, \moodle_database $db)
-    {
-        //TODO: restructure to allow usage of custom profile fields
-
-        if(is_int($user) && $user > 0){
-            $this->id = $user;
-            $this->object = $db->get_record('user',array('id'=>$user));
-            return true;
-        }
-
-        if(is_object($user) && isset($user->id) && $user->id > 0){
-            $this->id = $user->id;
-            $this->object = $user;
-            return true;
-        }
-
-        throw new exception\invalid_user_argument($user);
-    }
-
-    /**
-     * @var array
-     */
-    private $membership = array();
-
-    /**
-     * @var array
-     */
-    private $courses = array();
-
-    /**
-     * @var /stdclass
-     */
-    private $object;
+if($courseid == $SITE->id || !plugin_is_enabled()){
+    //do not allow editing for front page.
+    die();
 }
+
+$course = $DB->get_record('course', array('id' => $courseid));
+$groupsets = $DB->get_records('local_autogroup_set', array('courseid'=>$courseid));
+
+foreach($groupsets as $k => $groupset) {
+    $groupsets[$k] = new domain\autogroup_set($DB, $groupset);
+}
+
+$heading = \get_string('coursesettingstitle', 'local_autogroup', $course->shortname);
+
+global $PAGE;
+
+$PAGE->set_context($context);
+$PAGE->set_url(local_autogroup_renderer::URL_COURSE_MANAGE, array('courseid'=>$courseid));
+$PAGE->set_title($heading);
+$PAGE->set_heading($heading);
+$PAGE->set_pagelayout('incourse');
+$PAGE->set_course($course);
+
+$output = $PAGE->get_renderer('local_autogroup');
+
+
+echo $output->header();
+
+echo $output->intro_text(count($groupsets));
+
+echo $output->groupsets_table($groupsets);
+
+echo $output->add_new_groupset($courseid);
+
+echo $output->footer();
